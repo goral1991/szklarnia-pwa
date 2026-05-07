fcbaaeef2e54e2db7c57a31eb6fb4c5e0383e569
goral1991 <87304389+goral1991@users.noreply.github.com>
2026-05-07 08:44:01 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "szklarnia-cache-v1";
+const urlsToCache = [
+  "/",
+  "index.php",
+  "style.css",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/chart.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(async (cache) => {
+      for (const url of urlsToCache) {
+        try {
+          await cache.add(url);
+        } catch (e) {
+          console.warn("Nie dodano do cache:", url, e);
+        }
+      }
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
